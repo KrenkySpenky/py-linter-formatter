d9bb81c9452dc5c2a9ac5a0d6b1e32738f9bb9dc
Artem <167298119+KrenkySpenky@users.noreply.github.com>
2024-06-07 14:30:28 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,7 +8,6 @@ def format_linter_error(error: dict) -> dict:
         "source": "flake8"          
 }                                    
 
-
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     # write your code here
     return {
@@ -16,7 +15,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
         "path": file_path,                                              
         "status": "failed" if errors else "passed"                      
 }                                                                   
-
 
 def format_linter_report(linter_report: dict) -> list:
     # write your code here
